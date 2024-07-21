3c95d5825870f4612090c9b6f329a20b91c5c994
Adrian Manczak <manczakadrian@gmail.com>
2024-07-21 19:57:37 +0200

    testowaie-aplikacji-react

--- testing-intro/src/pages/Intro/components/PasswordValidator.js
+++ testing-intro/src/pages/Intro/components/PasswordValidator.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const PasswordValidator = () => {
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleValidateClick = () => {
+    const specialCharacters = ["!", "@", "#", "$", "%"];
+    let msg = "Hasło jest OK";
+    if (password.length > 8) {
+      msg = "Hasło jest za długie max 8 znaków";
+    } else if (password.length < 3) {
+      msg = "Hasło jest za krótkie min 3 znaki";
+    } else if (!specialCharacters.some((char) => password.includes(char))) {
+      msg = "Hasło powinno zawierać jeden ze znaków specjalnych ! @ # $ %";
+    }
+    setMessage(msg);
+  };
+
+  
   return (
     <article>
       <p>Password validator</p>
-      <input type="password" />
-      <p>Hasło jest za długie max 8 znaków</p>
-      <p>Hasło jest za krótkie min 3 znaki</p>
-      <p>Hasło powinno zawierać jeden ze znaków specjalnych ! @ # $ %</p>
-      <p>Hasło jest OK</p>
-      <button>Validate</button>
+
+      <input
+        type="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <p>{message}</p>
+      <button onClick={handleValidateClick}>Validate</button>
     </article>
   );
 };
